af3503a307a5d284806c1f7bd02ab5e992eb56f8
mundus <mundus@pc6.home>
2023-11-10 18:09:39 +0100

    Initialized genetic algorithm sample backpack data & read this data

--- metaheuristics_lab_demos/genetic_algorithm/main.c
+++ metaheuristics_lab_demos/genetic_algorithm/main.c
@@ -2,7 +2,7 @@
 #include "bizzare_backpack.h"
 
 int main() {
-    printf("Place for genetic algorighm demos batch run");
+    printf("Place for genetic algorighm demos batch run\n");
     backpack_t bizzareBackpack = get_bizzare_backpack();
     for (unsigned int i = 0; i < bizzareBackpack.num_available; i++) {
         printf("%d %s\n", i, bizzareBackpack.available_names[i]);
